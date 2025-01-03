0c999b6e2656fb077369f0e924247a266482c09e
Harsh Narayan Jha <harshnj@proton.me>
2025-01-03 15:18:55 +0530

    initial game loop, resume game, and completed the bare minimum movement

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import json
 import os
 import subprocess
 from time import sleep
@@ -7,7 +8,7 @@ import dotenv
 import google.generativeai as genai
 from PIL import Image
 
-from helpers import hold_a, press_a, press_b, press_d, press_l, press_r, press_u, release_a, take_screenshot
+from helpers import pre_game, press_a, press_b, press_d, press_l, press_m, press_n, press_r, press_u, start_game_menu, take_screenshot
 
 API_KEY = dotenv.get_key(dotenv.find_dotenv(), "API_KEY")
 ROM_PATH = "./Pokemon - Emerald Version (USA, Europe).gba"
@@ -34,8 +35,8 @@ and also a description of the actions performed. Respond in JSON only. Return an
 and a key called `description` with the description of the move. Contain as much key presses as possible in one screenshot. DO NOT return anything else.
 Don't use markdown syntax, only JSON.
 
-Valid Buttons are: a, b, u, d, l, r
-i.e. A, B, Up, Down, Left, Right
+Valid Buttons are: a, b, u, d, l, r, m, n
+i.e. A, B, Up, Down, Left, Right, Start, Select
 
 Use this JSON schema:
 
@@ -44,55 +45,55 @@ Return: Move
 """.strip()
 
 playing = True
-subprocess.Popen(["mgba-qt", ROM_PATH], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
 chat = model.start_chat()
 response = chat.send_message([PROMPT])
+# print(response.text)
+
+# response = chat.send_message(
+#     ["Okay let's start with a test. The picture is in the lab of proff oak with pokeballs in front of the character"],
+#     generation_config=genai.GenerationConfig(response_mime_type="application/json", response_schema=Move),
+# )
+
+subprocess.Popen(["mgba-qt", ROM_PATH], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
 for i in range(5, -1, -1):
     print(f"Starting Gameplay control in {i} seconds")
     sleep(1)
 
 print("Spawing dotoold in background")
+os.system("pkill dotoold")
 os.system("dotoold &")
 
+# Starting Game
+print(response.text)
+print()
+
+start_game_menu()
+
+if False:
+    # only if new save file
+    pre_game()
+
+sleep(5)
+
 while playing:
-    print(response.text)
-    print()
-
-    sleep(2)
-    press_a()
-    sleep(8)
-    press_a()
-    sleep(3)
-    press_a()
-
-    print("Opening passed!")
-    print("Starting Professor scene!")
-
-    hold_a()
-    for _ in range(20):
-        sleep(2)
-        press_b()
-    release_a()
-
-    print("Selecting Character")
-
-    sleep(8)
-
-    # ask gemini for character
-    # press keys
-    #
-    # take screenshot
+    print("Starting AI Gameplay Loop".center(50, "*"))
     img_path = take_screenshot()
 
+    print("Asking AI the screenshot....")
     response = chat.send_message(
         Image.open(img_path), generation_config=genai.GenerationConfig(response_mime_type="application/json", response_schema=Move)
     )
 
+    print("Got Response")
     print(response)
+    print()
+    print("Making steps...")
 
-    for button in response["buttons"]:
+    move = json.loads(response.text)
+
+    for button in move["buttons"]:
         match button.lower():
             case "a":
                 press_a()
@@ -106,5 +107,13 @@ while playing:
                 press_l()
             case "r":
                 press_r()
+            case "m":
+                press_m()
+            case "n":
+                press_n()
 
-    sleep(500)
+        sleep(5)
+
+    print("Waiting 15 seconds before next screenshot...")
+    print("\n\n")
+    sleep(15)
--- helpers.py
+++ helpers.py
@@ -71,6 +71,24 @@ def press_r():
     subprocess.run(["dotoolc"], stdin=psu.stdout, check=True)
 
 
+def press_m():
+    print("Pressing Start")
+    psd = subprocess.Popen(["echo", "keydown m"], stdout=subprocess.PIPE)
+    psu = subprocess.Popen(["echo", "keyup m"], stdout=subprocess.PIPE)
+    subprocess.run(["dotoolc"], stdin=psd.stdout, check=True)
+    sleep(0.5)
+    subprocess.run(["dotoolc"], stdin=psu.stdout, check=True)
+
+
+def press_n():
+    print("Pressing Select")
+    psd = subprocess.Popen(["echo", "keydown n"], stdout=subprocess.PIPE)
+    psu = subprocess.Popen(["echo", "keyup n"], stdout=subprocess.PIPE)
+    subprocess.run(["dotoolc"], stdin=psd.stdout, check=True)
+    sleep(0.5)
+    subprocess.run(["dotoolc"], stdin=psu.stdout, check=True)
+
+
 def take_screenshot():
     print("Taking screenshot")
     ps = subprocess.Popen(["echo", "key f12"], stdout=subprocess.PIPE)
@@ -82,3 +100,60 @@ def take_screenshot():
     latest = max(nums)
     ss = f"{ROM_NAME}-{latest}.png"
     return ss
+
+
+def start_game_menu():
+    sleep(2)
+    press_a()
+    sleep(8)
+    press_a()
+    sleep(3)
+    press_a()
+
+    print("Opening passed!")
+
+
+def pre_game():
+    start_game_menu()
+
+    print("Starting Professor scene!")
+    hold_a()
+    for _ in range(20):
+        sleep(2)
+        press_b()
+        release_a()
+
+    print("Selecting Character")
+
+    sleep(10)
+
+    # select Male Character
+    press_a()
+    sleep(8)
+    press_a()
+    sleep(3)
+
+    # type out ASH
+    press_a()
+
+    for _ in range(3):
+        press_d()
+        for _ in range(6):
+            press_r()
+            press_a()
+            for _ in range(5):
+                press_l()
+                press_u()
+                press_a()
+
+    press_m()
+    press_a()
+
+    sleep(2)
+    press_a()
+
+    hold_a()
+    for _ in range(10):
+        sleep(2)
+        press_b()
+        release_a()
